a3a7f22e99fe17f9d2da6aaad52c0ce7ef74359a
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-10-23 05:04:35 +0530

    Signup  Page Done

--- client/src/auth/Signup.tsx
+++ client/src/auth/Signup.tsx
@@ -1,5 +1,130 @@
-const Signup = () => {
-  return <div>Signup</div>;
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Loader2, LockKeyhole, Mail, PhoneOutgoing, User } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
+type SignupInputState = {
+  fullname: string;
+  email: string;
+  password: string;
+  contact: number;
 };
 
+const Signup = () => {
+  const [input, setInput] = useState<SignupInputState>({
+    fullname: "",
+    email: "",
+    password: "",
+    contact: "",
+  });
+
+  const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+  const signupSubmitHandler = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(input);
+  };
+  const loading = false;
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen ">
+        <form
+          action=""
+          onSubmit={signupSubmitHandler}
+          className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200  mx-4 "
+        >
+          <div className="mb-4">
+            <h1 className=" font-bold text-2xl">Indian Cousins </h1>
+          </div>
+          <div className="mb-4">
+            <div className=" relative">
+              <Input
+                type="text"
+                placeholder="Enter Your Full Name"
+                className="pl-10 focus-visible:ring-1"
+                value={input.fullname}
+                onChange={changeEventHandler}
+                name="fullname"
+              />
+              <User className=" absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+            </div>
+          </div>
+          <div className="mb-4">
+            <div className=" relative">
+              <Input
+                type="email"
+                placeholder="Enter Your Email"
+                className="pl-10 focus-visible:ring-1"
+                value={input.email}
+                onChange={changeEventHandler}
+                name="email"
+              />
+              <Mail className=" absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <div className="relative">
+              <Input
+                type="password"
+                placeholder="Enter Your Password"
+                className="pl-10 focus-visible:right-1"
+                onChange={changeEventHandler}
+                value={input.password}
+                name="password"
+              />
+              <LockKeyhole className=" absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+            </div>
+          </div>
+          <div className="mb-4">
+            <div className="relative">
+              <Input
+                type="text"
+                placeholder="Contact"
+                name="contact"
+                value={input.contact}
+                onChange={changeEventHandler}
+                className="pl-10 focus-visible:ring-1"
+              />
+              <PhoneOutgoing className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+              {/* {errors && (
+                <span className="text-xs text-red-500">{errors.contact}</span>
+              )} */}
+            </div>
+          </div>
+          <div className="mb-10">
+            {loading ? (
+              <Button
+                disabled
+                className="bg-orange hover:bg-hoverOrange w-full "
+              >
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please Wait
+              </Button>
+            ) : (
+              <Button
+                className="bg-orange hover:bg-hoverOrange w-full "
+                type="submit"
+              >
+                Signup
+              </Button>
+            )}
+          </div>
+
+          <Separator />
+          <p className="mt-2">
+            Already have an account?{" "}
+            <Link to="/login" className="text-blue-500">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+    </>
+  );
+};
 export default Signup;
