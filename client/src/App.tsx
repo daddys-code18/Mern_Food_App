c89ad6c3b92cc1e957b875f48592ae0afc785f44
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-10-25 02:08:34 +0530

    Admin Add Menu and Edit Menu  feature and Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,7 @@ import SearchPage from "./components/SearchPage";
 import RestaurantDetail from "./components/RestaurantDetail";
 import Cart from "./components/Cart";
 import Restaurant from "./admin/Restaurant";
+import AddMenu from "./admin/AddMenu";
 
 const appRouter = createBrowserRouter([
   {
@@ -39,6 +40,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/admin/restaurant",
         element: <Restaurant />,
+      },
+      {
+        path: "/admin/menu",
+        element: <AddMenu />,
+      },
+      {
+        path: "/admin/orders",
+        // element: <Restaurant />,
       },
     ],
   },
---
+++ client/src/types/restaurantType.ts
@@ -0,0 +1,7 @@
+export type MenuItem = {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  image: string;
+};
