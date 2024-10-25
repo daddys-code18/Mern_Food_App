8fce29d622acf1569028ae1058afff10530a7680
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-10-26 04:36:04 +0530

    User Login and VerifyEmail  Api

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -41,3 +41,73 @@ export const signUp = async (req: Request, res: Response) => {
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+// User Login API
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+
+    generateToken(res, user);
+    user.lastLogin = new Date();
+    await user.save();
+
+    // send user without passowrd
+    const userWithoutPassword = await User.findOne({ email }).select(
+      "-password"
+    );
+    return res.status(200).json({
+      success: true,
+      message: `Welcome back ${user.fullname}`,
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const verifyEmail = async (req: Request, res: Response) => {
+  try {
+    const { verificationCode } = req.body;
+
+    const user = await User.findOne({
+      verificationToken: verificationCode,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    }).select("-password");
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid or expired verification token",
+      });
+    }
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "Email verified successfully.",
+      user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
