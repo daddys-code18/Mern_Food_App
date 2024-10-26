27bf124858ba2265a6f84fb2baee7fab6fb06454
Pavan Kafare <122674140+daddys-code18@users.noreply.github.com>
2024-10-26 15:57:29 +0530

    User All  Routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { config } from "dotenv";
 import { connectDB } from "./db/conectDB.js";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRoute from "./routes/user.route.js";
 
 config({ path: "./.env" });
 
@@ -9,6 +13,20 @@ const mongoURI = process.env.MONGO_URI || "";
 connectDB(mongoURI);
 
 const app = express();
+
+// default middleware for any mern project
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173/",
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+// api
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
